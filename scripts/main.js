45fa13964229f60890d30cf4005f069f340f1fda
Grzegorz Dubiel <greg2012201@gmail.com>
2020-09-15 09:40:01 +0200

    work on smooth scroll function

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,49 @@
-document.querySelector('.about').addEventListener('click', () => {
+/* document.querySelector('.about').addEventListener('click', () => {
     setTimeout(() =>
         document.querySelector('p').scrollIntoView({
             behavior: 'smooth'
         }), 2000)
-});
+}); */
+
+// przykłady https://stackoverflow.com/questions/17733076/smooth-scroll-anchor-links-without-jquery
+
+//https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame - dobra rzecz do użycia ws animacji 
+
+// znaleźć pozycję elementu do którego będzie się scrolować 
+// trzeba znaleźć range odległośc ile jest od góry do punktu
+
+// na zdjęcia w galerii dodajemy powiększanie lekkie po :hover tak jak w projekcie z samurajem 
+
+console.log(document.documentElement.scrollLeft);
+console.log(window.pageYOffset);
+const p = document.querySelector('p');
+
+const move = () => { // spoko funkcja na przsuwanie
+    let px = 0.1;
+    /* let speed */
+
+
+    let intervalIndex = setInterval((index) => {
+        window.scroll(0, px++), 100000;
+        console.log(window.scrollY); // trzeba użyć zaokrąglenia
+        console.log(p.offsetLeft);
+        stopMoving()
+    })
+
+
+    return intervalIndex
+
+
+}
+document.querySelector('.about').addEventListener('click', move);
+
+
+const stopMoving = () => {
+
+
+
+    if (p.offsetleft == window.screenY - 200) console.log('ok');
+
+
+
+}
