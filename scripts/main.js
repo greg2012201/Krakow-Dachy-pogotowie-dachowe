6f48b8be13f2547a9773fcd32cf38ce026918c53
Grzegorz Dubiel <greg2012201@gmail.com>
2020-09-17 08:57:38 +0200

    main.js updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,30 @@
-/* document.querySelector('.about').addEventListener('click', () => {
-    setTimeout(() =>
-        document.querySelector('p').scrollIntoView({
-            behavior: 'smooth'
-        }), 2000)
-}); */
+const p = document.querySelector('p'); //funkcja musi być uniwersalna dla przewijania w górę i w dół jeszcze to ogarnąć
 
-// przykłady https://stackoverflow.com/questions/17733076/smooth-scroll-anchor-links-without-jquery
+const move = (e) => {
+    let px = 1;
+    let speed = 1;
 
-//https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame - dobra rzecz do użycia ws animacji 
+    e.preventDefault()
 
-// znaleźć pozycję elementu do którego będzie się scrolować 
-// trzeba znaleźć range odległośc ile jest od góry do punktu
+    let intervalIndex = setInterval(() => {
+        speed++
+        window.scroll(0, px += speed);
 
-// na zdjęcia w galerii dodajemy powiększanie lekkie po :hover tak jak w projekcie z samurajem 
 
-/* console.log(document.documentElement.scrollLeft);
-console.log(window.pageYOffset); */
-const p = document.querySelector('p');
-
-const move = () => { // spoko funkcja na przsuwanie
-    let px = 1; // to powinno w miare działać
-    let speed = 4;
+        let distance = (Math.round(p.offsetTop)) - (Math.round(window.scrollY))
+        let pPosition = (Math.round(p.offsetTop));
+        let scrollYPosition = (Math.round(window.scrollY) - 1)
 
 
 
+        if (distance <= 0) {
+
+            window.scrollTo(0, pPosition)
+            stopMoving(intervalIndex)
+        }
+    }, 10)
 
 
-
-    let intervalIndex = setInterval((e) => { // można napsać funkcję która przeszukuje pixel po pixelu albo odwrócić obliczenie z windows scrol
-
-            window.scroll(0, px += speed);
-            // trzeba ustalić dokładną pozycję znaleźć metodę która odszukuje czy w polu widzenia znajduje się element
-            console.log(px);
-            /* console.log(speed); */
-            // ogónie napisać to bardziej przejrzyście może w classie ? 
-            let distance = (Math.round(p.offsetTop)) - (Math.round(window.scrollY))
-            let pPosition = (Math.round(p.offsetTop));
-            let scrollYPosition = (Math.round(window.scrollY) - 1)
-            /*   console.log(pPosition);
-              console.log(scrollYPosition); */
-            console.log(distance);
-
-            if ( /* pPosition == scrollYPosition */ distance <= 0) /* console.log('działa'); */ stopMoving(intervalIndex)
-        },
-        1)
-    // trzeba jeszcze dodać usunięcie linka
-    console.log(p.offsetTop);
-
-    /* return intervalIndex */
 
 
 }
