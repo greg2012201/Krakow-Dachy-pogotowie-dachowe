b204d1217f08a38c6ef81ee6ce497b56082b9947
Grzegorz Dubiel <greg2012201@gmail.com>
2020-09-19 12:41:53 +0200

    smooth scroling on each direction updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,64 @@
-const p = document.querySelector('p'); //funkcja musi być uniwersalna dla przewijania w górę i w dół jeszcze to ogarnąć
+const p = document.querySelectorAll('p'); //funkcja musi być uniwersalna dla przewijania w górę i w dół jeszcze to ogarnąć
 
-const move = (e) => {
-    let px = 1;
+const navBtns = document.querySelectorAll('a');
+
+let px = 1;
+
+const move = (e, obj) => {
+    e.preventDefault();
+
+
+
+
+
+    /*   elementsData.forEach(element => console.log(element.dataset)); */
+
+
+    scrollFinish = document.querySelector(`p[data-set = "${e.target.dataset.set}"]`).offsetTop;
+
+    console.log(scrollFinish);
+
+
     let speed = 1;
-
-    e.preventDefault()
 
     let intervalIndex = setInterval(() => {
         speed++
+        console.log(px);
         window.scroll(0, px += speed);
+        // zrobić tak że if position większa niż px to wtedy += speed
+        // if position mniejsza niż px to wtedy -= speed
+        /* console.log(Math.round(scrollFinish)); */
 
+        let distance = (Math.round(scrollFinish)) - (Math.round(window.scrollY)); // zastanowić się czy zaokrąglenie jest tutaj potrzebne
+        let pPosition = (Math.round(scrollFinish));
 
-        let distance = (Math.round(p.offsetTop)) - (Math.round(window.scrollY))
-        let pPosition = (Math.round(p.offsetTop));
-        let scrollYPosition = (Math.round(window.scrollY) - 1)
+        if (px > scrollFinish) {
+
+            /* console.log('ok'); */
+        }
+
 
 
 
         if (distance <= 0) {
 
-            window.scrollTo(0, pPosition)
-            stopMoving(intervalIndex)
+            window.scrollTo(0, pPosition);
+            stopMoving(intervalIndex);
         }
-    }, 10)
+    }, 10);
 
 
 
 
 }
-document.querySelector('.about').addEventListener('click', move);
+/* document.querySelector('.about').addEventListener('click', move); */
+
+navBtns.forEach(btn => btn.addEventListener('click', move));
 
 
 const stopMoving = (intervalIndex) => {
 
-    clearInterval(intervalIndex)
+    clearInterval(intervalIndex);
 
 
 
