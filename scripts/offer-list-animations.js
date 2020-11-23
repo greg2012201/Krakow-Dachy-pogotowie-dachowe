84ac0f616da6f3cd2e84c3132ffe690484b8a10d
Grzegorz Dubiel <greg2012201@gmail.com>
2020-11-23 12:01:23 +0100

    add animations to offer lists

---
+++ scripts/offer-list-animations.js
@@ -0,0 +1,46 @@
+const button = document.querySelectorAll('.list-wrapper__title');
+/* const li = document.querySelectorAll('.list-wrapper__item'); */
+document.querySelectorAll('.list-wrapper').forEach((wrapp, i) => {
+    wrapp.dataset.key = i;
+    wrapp.firstElementChild.dataset.key = i;
+
+});
+/* const list = document.querySelectorAll('.list-wrapper__list'); */
+
+
+
+
+/* if e.target.parentNode === wrapper */
+
+
+
+
+button.forEach(btn => btn.addEventListener('click', (e) => {
+
+
+    if (e.target.parentNode.dataset.key === e.target.dataset.key) {
+        let listHeight;
+        const btnHeight = e.target.getBoundingClientRect().height;
+        console.log(btnHeight);
+        e.target.parentNode.querySelectorAll('.list-wrapper__list').forEach(element => {
+
+            element.classList.toggle('list-wrapper__list--active')
+
+            listHeight = element.getBoundingClientRect().height;
+
+            console.log(listHeight);
+            e.target.parentNode.style.height = `${listHeight+btnHeight}px`
+        });
+
+        /* e.target.parentNode.classList.toggle("list-wrapper--ative") */
+
+
+
+    }
+
+
+
+    /* document.querySelectorAll('.list-wrapper__list').forEach(element => element.classList.toggle('list-wrapper__list--active')) */
+
+
+}))
