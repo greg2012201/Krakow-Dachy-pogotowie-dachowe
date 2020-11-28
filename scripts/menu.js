0253102114d979bca23f4ef7a5eb99d86453d34d
Grzegorz Dubiel <greg2012201@gmail.com>
2020-11-28 14:54:55 +0100

    update menu.js add closeWhenClickOutOfMenu function

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,11 +1,57 @@
 const burger = document.querySelector('.burger');
 const dropDownMenu = document.querySelector('.menu');
 
-const activate = function () {
+let menuIsOpen = false;
+/* 
+let whereClicked;
+const whereIsClick = (e) => {
+    whereClicked = e.target;
+}
+// TREBA DAĆ FUNCKJĘ WHERE I CLICKED I DO ACTIVATE TO DAĆ FUNKCJA TA MA ZWRÓCIĆ E.TARGET 
+document.addEventListener('click', whereIsClick); */
 
-    this.classList.toggle('burger--active');
+const toggle = function (e) {
+    menuIsOpen = true;
+    e.classList.toggle('burger--active');
+
+
+
     dropDownMenu.classList.toggle('menu--droped');
 
-}
 
-burger.addEventListener('click', activate);
+
+};
+let isDroped = false;
+const active = function (e) {
+
+    toggle(this);
+    isDroped = true
+
+
+
+};
+
+
+
+burger.addEventListener('click', active);
+
+;
+
+const closeWhenClickOutOfMenu = function (e) {
+
+    const childs = [...dropDownMenu.querySelectorAll('*')];
+
+    const isChild = childs.find(element => element === e.target)
+    console.log(isChild);
+
+
+    if (menuIsOpen || isChild) {
+
+        return menuIsOpen = false;
+    } else if (dropDownMenu.classList.contains('menu--droped') && !menuIsOpen) {
+
+        toggle(burger);
+    }
+
+};
+document.addEventListener('click', closeWhenClickOutOfMenu)
