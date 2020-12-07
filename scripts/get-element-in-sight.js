d715117ff863971916d757cff295d9346324cf07
Grzegorz Dubiel <greg2012201@gmail.com>
2020-12-07 10:53:54 +0100

    optimize variables in detectAndChangeClassOfElement get-element-in-sight.js

--- scripts/get-element-in-sight.js
+++ scripts/get-element-in-sight.js
@@ -1,8 +1,9 @@
 const detectAndChangeClassOfElement = (elements) => {
     const vieportHeight = window.innerHeight;
     elements.forEach(element => {
-        const top = element.getBoundingClientRect().top;
-        const bottom = element.getBoundingClientRect().bottom;
+        const rect = element.getBoundingClientRect();
+        const top = rect.top;
+        const bottom = rect.bottom;
         top - vieportHeight <= 0 && bottom >= 0 ? element.classList.add('--into-view') : element.classList.remove('--into-view');
     });
 
