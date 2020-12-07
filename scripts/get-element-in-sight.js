025606d26009379d5e5421dd94ed225e813fbbf9
Grzegorz Dubiel <greg2012201@gmail.com>
2020-12-07 09:25:04 +0100

    add semicolon & remove test invoke of setElementsToView

--- scripts/get-element-in-sight.js
+++ scripts/get-element-in-sight.js
@@ -3,14 +3,12 @@ const detectAndChangeClassOfElement = (elements) => {
     elements.forEach(element => {
         const top = element.getBoundingClientRect().top;
         const bottom = element.getBoundingClientRect().bottom;
-        top - vieportHeight <= 0 && bottom >= 0 ? element.classList.add('--into-view') : element.classList.remove('--into-view')
+        top - vieportHeight <= 0 && bottom >= 0 ? element.classList.add('--into-view') : element.classList.remove('--into-view');
     });
 
 
-}
+};
 const setElementsToView = (elements) => {
 
     return window.addEventListener('scroll', () => detectAndChangeClassOfElement(elements));
 };
-
-setElementsToView(sections)
