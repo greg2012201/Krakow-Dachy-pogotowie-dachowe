5083fcb8b4a63a4eb9a9b5670c5e52c4b391d529
Grzegorz Dubiel <greg2012201@gmail.com>
2020-12-07 09:19:46 +0100

    rename sections.js to get-element-in-sight & create setElementsToView, detectAndChangeClassOfElement

---
+++ scripts/get-element-in-sight.js
@@ -0,0 +1,16 @@
+const detectAndChangeClassOfElement = (elements) => {
+    const vieportHeight = window.innerHeight;
+    elements.forEach(element => {
+        const top = element.getBoundingClientRect().top;
+        const bottom = element.getBoundingClientRect().bottom;
+        top - vieportHeight <= 0 && bottom >= 0 ? element.classList.add('--into-view') : element.classList.remove('--into-view')
+    });
+
+
+}
+const setElementsToView = (elements) => {
+
+    return window.addEventListener('scroll', () => detectAndChangeClassOfElement(elements));
+};
+
+setElementsToView(sections)
