49239bb26c0cae413297be2ddf2d8862f35a083b
Grzegorz Dubiel <greg2012201@gmail.com>
2020-12-08 08:31:49 +0100

    Add getClass function & modify source code of detectAndChangeClassOfElement

--- scripts/get-element-in-sight.js
+++ scripts/get-element-in-sight.js
@@ -1,15 +1,23 @@
 const detectAndChangeClassOfElement = (elements) => {
     const vieportHeight = window.innerHeight;
     elements.forEach(element => {
+        const classOfElement = `${getClass(element)}--into-view`;
+        console.log(classOfElement);
         const rect = element.getBoundingClientRect();
         const top = rect.top;
         const bottom = rect.bottom;
-        top - vieportHeight <= 0 && bottom >= 0 ? element.classList.add('--into-view') : element.classList.remove('--into-view');
+        console.log(bottom);
+        top - vieportHeight <= 0 && bottom >= 0 ? element.classList.add(classOfElement) : element.classList.remove(classOfElement);
     });
 
 
+
+
 };
+const getClass = (element) => element.classList.item(0);
 const setElementsToView = (elements) => {
 
     return window.addEventListener('scroll', () => detectAndChangeClassOfElement(elements));
 };
+
+setElementsToView(sections)
