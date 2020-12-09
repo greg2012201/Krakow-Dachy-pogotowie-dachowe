587ad49cd95a0c9b159a38b1686cd3a5282843c3
Grzegorz Dubiel <greg2012201@gmail.com>
2020-12-09 10:07:14 +0100

    optimaze source code: create new functions for single responsibility

--- scripts/get-element-in-sight.js
+++ scripts/get-element-in-sight.js
@@ -1,25 +1,35 @@
-const detectAndChangeClassOfElement = (elements, modifier) => {
+const getElementToAddModifier = (elements, modifier) => {
 
-
-
-    const vieportHeight = window.innerHeight;
     let arr;
     elements.map(element => [...element]).sort((a, b) => arr = a.concat(b));
-    arr.forEach(element => {
-        console.log(element);
-        const rect = element.getBoundingClientRect();
-        const top = rect.top;
-        const bottom = rect.bottom;
-        top - vieportHeight <= 0 && bottom >= 0 ? setModifier(element, modifier) : removeModifier(element, modifier);
-    });
-
+    return modifierManage(arr, modifier)
 };
+const modifierManage = (elements, modifier) => {
+    elements.forEach(element => {
+
+        if (isInView(getParameters(element))) setModifier(element, modifier);
+        else removeModifier(element, modifier);
+    });
+};
+const getParameters = (element) => {
+    const vieportHeight = window.innerHeight;
+    const rect = element.getBoundingClientRect();
+    return {
+        top: rect.top,
+        bottom: rect.bottom,
+        vieport: vieportHeight,
+    };
+};
+
+const isInView = (parameters) => parameters.top - parameters.vieport <= 0 && parameters.bottom >= 0;
+
+
 const getClass = (element, index) => element.classList.item(index);
 const setModifier = (element, modifier) => element.classList.add(`${getClass(element, 0)}${modifier}`);
 
 const removeModifier = (element, modifier) => {
     element.classList.remove(`${getClass(element, 0)}${modifier}`);
-}
+};
 const addModifier = (elements, modifier) => {
-    return window.addEventListener('scroll', () => detectAndChangeClassOfElement(elements, modifier));
+    return window.addEventListener('scroll', () => getElementToAddModifier(elements, modifier));
 };
