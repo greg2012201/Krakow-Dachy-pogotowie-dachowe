e40a16be0fbd4264644db92a7a7d280b03468bbd
Grzegorz Dubiel <greg2012201@gmail.com>
2020-12-11 08:47:33 +0100

    Remove sections-animations.js $ copy code from it to main.js

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+const images = document.querySelectorAll('.article__image');
+const text = document.querySelectorAll('.article__content');
+addModifier([images, text], '--into-view');
--- scripts/sections-animations.js
+++
@@ -1,3 +0,0 @@
-const images = document.querySelectorAll('.article__image');
-const text = document.querySelectorAll('.article__content');
-addModifier([images, text], '--into-view');
