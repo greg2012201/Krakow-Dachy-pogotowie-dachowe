d3597c36c07b7876f13ed9900c6459315a1bc9c1
Grzegorz Dubiel <greg2012201@gmail.com>
2020-12-21 16:34:14 +0100

    Remove infinite move animationa and add transition when title and slogan into view

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
 const images = document.querySelectorAll('.article__image');
 const text = document.querySelectorAll('.article__content');
+const title = document.querySelector('.header__title');
+const slogan = document.querySelector('.header__slogan');
 addModifier([images, text], '--into-view');
+addModifier([title, slogan], '--into-view', 1);
