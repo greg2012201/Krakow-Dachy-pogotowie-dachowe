e063236ef93b8ba04a13205c280a58d7e2affda6
Grzegorz Dubiel <greg2012201@gmail.com>
2020-12-23 10:39:10 +0100

    Remove death code line

--- scripts/resize-animation-stopper.js
+++ scripts/resize-animation-stopper.js
@@ -2,9 +2,11 @@ const page = document.querySelector('.page');
 let resizeTimer;
 
 window.addEventListener('resize', () => {
-    console.log('wywołanie');
     page.classList.add('page--resize-animation-stopper');
     clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => page.classList.remove('page--resize-animation-stopper'), 400);
+    resizeTimer = setTimeout(() => {
+        page.classList.remove('page--resize-animation-stopper');
+    }, 400);
+
 
 })
