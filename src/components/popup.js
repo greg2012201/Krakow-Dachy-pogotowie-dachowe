da3e8f54d72cd3aadde8d1eac5e61bda8dea4773
Grzegorz Dubiel <greg2012201@gmail.com>
2021-02-02 10:03:38 +0100

    Optimize code

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,22 +1,34 @@
 const popup = document.querySelector('.popup');
 const link = document.querySelector('.form__checkbox--link');
 const popupBtn = document.querySelector('.popup__button');
-let isOpen;
+let isOpen = false;
+
 
 
 const closeWhenClickOutOfPopup = (e) => {
 
+
     if (e.target !== popup && e.target.parentNode !== popup && isOpen && e.target !== link)
-        close();
+
+    {
+        popupManage();
+
+    }
+
 
 }
+
 window.addEventListener('click', (e) => closeWhenClickOutOfPopup(e));
 
 const popupManage = (e) => {
-    e.preventDefault();
-    if (!isOpen)
+    if (e) {
+        e.preventDefault();
+    }
+    if (!isOpen) {
         open();
-    else close();
+    } else {
+        close();
+    }
 
 
 
