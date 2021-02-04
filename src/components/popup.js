1ba56641100868c6551a894ff303a6fb2eb13ddf
Grzegorz Dubiel <greg2012201@gmail.com>
2021-02-04 10:00:16 +0100

    Fist working version of the popup

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,9 +1,15 @@
+import {
+    getElementPosition
+} from '../tools/getElementPosition'
+
+
 const popup = document.querySelector('.popup');
 const link = document.querySelector('.form__checkbox--link');
 const popupBtn = document.querySelector('.popup__button');
 let isOpen = false;
 
 
+/* POPUP OPEN/CLOSE */
 
 const closeWhenClickOutOfPopup = (e) => {
 
@@ -13,33 +19,110 @@ const closeWhenClickOutOfPopup = (e) => {
     }
 }
 window.addEventListener('click', (e) => closeWhenClickOutOfPopup(e));
+const getPopupPosition = () => {
+    return {
+        height: getElementPosition(popup).height,
+        top: getElementPosition(popup).top,
+        bottom: getElementPosition(popup).bottom,
+    }
+
+}
+const getLinkPosition = () => {
+
+    return {
+        top: getElementPosition(link).top,
+        bottom: getElementPosition(link).bottom,
+    }
+
+}
 
 const popupManage = () => {
 
+
+    const popupHeight = getPopupPosition().height;
+    const linkPosition = getLinkPosition();
+    const up = linkPosition.top - popupHeight <= 0;
+    const down = innerHeight - (linkPosition.bottom - popupHeight) <= 0;
+
+
     if (!isOpen) {
-        return open();
+        popupActive();
+        if (up) {
+            openDown();
+
+        } else if (down) {
+            openUp();
+            closeDown();
+        } else {
+            openUp();
+
+        }
+
     } else {
-        return close();
+        console.log(isOpen);
+        closeUp();
+        closeDown();
+        popupDisactive();
+
     }
 
 
 
-}
-const close = () => {
-    popup.classList.remove('popup--active');
-    isOpen = false;
 
 }
+document.addEventListener('scroll', () => {
 
 
-const open = () => {
-    popup.classList.add('popup--active');
+    const popupPosition = getPopupPosition();
+    const up = popupPosition.top <= 0;
+    const down = innerHeight - popupPosition.bottom <= 0;
+
+    if (isOpen) {
+        if (up) {
+            openDown();
+            closeUp();
+
+        } else if (down) {
+            openUp();
+            closeDown();
+        }
+
+
+    }
+});
+
+const popupActive = () => {
     isOpen = true;
+    popup.classList.add('popup--active');
+}
+const popupDisactive = () => {
+    isOpen = false;
+    popup.classList.remove('popup--active');
+    closeUp();
+    closeDown();
 }
 
+const closeUp = () => {
+
+    popup.classList.remove('popup--up');
+}
+const closeDown = () => {
+
+    popup.classList.remove('popup--down');
+}
+
+
+const openUp = () => {
+
+    popup.classList.add('popup--up');
+}
+const openDown = () => {
+
+    popup.classList.add('popup--down');
+}
 
 link.addEventListener('click', (e) => {
     e.preventDefault();
     popupManage();
 });
-popupBtn.addEventListener('click', close);
+popupBtn.addEventListener('click', popupDisactive);
