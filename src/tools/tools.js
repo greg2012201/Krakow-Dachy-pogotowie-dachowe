f6e540fea040f9b19ca2d95341575ce18296f6f1
Grzegorz Dubiel <greg2012201@gmail.com>
2021-02-25 10:10:43 +0100

    Remove smooth scroll on wheel

--- src/tools/tools.js
+++ src/tools/tools.js
@@ -1,12 +1,12 @@
 import {
     addModifier
-} from './get-element-in-sight';
+} from './get-element-in-sight'; // get musi zwracać i to co zwróci musi wrzucać do add modifier osobno
 import {
     animationStopperInit
 } from './resize-animation-stopper';
 
-
-
+// może dodaj tutaj domcontent loaded 
+// DODAĆ OBSŁUGĘ GDY COŚ JEST NULL I NIE CHWYCONE PRZEZ QUERY SELECTOR
 const images = document.querySelectorAll('.article__image');
 const text = document.querySelectorAll('.article__content');
 const title = document.querySelector('.header__title');
