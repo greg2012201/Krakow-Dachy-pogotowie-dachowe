50ece1f2aaa44180ccbdbf0f5f3a102473813537
Grzegorz Dubiel <greg2012201@gmail.com>
2021-03-22 11:08:32 +0100

    Error management & Array method remove the 'flat' Array method call & add error management

--- src/tools/modifierManage.js
+++ src/tools/modifierManage.js
@@ -1,5 +1,6 @@
 export const modifierManage = (elements, modifier, remove) => {
-  elements.flat().map((element) => {
+  argValidation(elements, modifier, remove);
+  elements.map((element) => {
     if (!remove) {
       element.classList.add(`${getClass(element, 0)}${modifier}`);
     } else {
@@ -8,4 +9,27 @@ export const modifierManage = (elements, modifier, remove) => {
   });
 };
 
+const argValidation = (elements, modifier, remove) => {
+  const isElements =
+    Array.isArray(elements) && elements === '[object NodeList]';
+  const regex = /--[a-z0-9]/;
+  const isModifier = regex.test(modifier);
+  const isRemove = typeof remove === 'boolean';
+
+  if (!isElements) {
+    throw Error(
+      `The argument: "${elements}" is not valid. Provide a valid value, the expected array of html elements such as div etc.`
+    );
+  }
+  if (!isModifier) {
+    throw Error(
+      `The argument: "${modifier}" is not valid. Provide a valid value, for example:'--modifier'`
+    );
+  }
+  if (!isRemove) {
+    throw Error(
+      `The argument: "${remove}" is not valid. Provide a valid value: true or false`
+    );
+  }
+};
 const getClass = (element, index) => element.classList.item(index);
