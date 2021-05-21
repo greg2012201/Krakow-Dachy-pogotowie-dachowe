c0edc7aa42ddb25941f3429876fc15cb89df38f9
Grzegorz Dubiel <greg2012201@gmail.com>
2021-05-21 22:36:31 +0200

    Update dependencies and the webpack config

--- configuration/webpack.dev.config.js
+++ configuration/webpack.dev.config.js
@@ -1,43 +1,31 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const webpack = require('webpack');
 const webpackConfiguration = require('../webpack.config');
 const ip = require('ip');
 
 module.exports = merge(webpackConfiguration, {
+  mode: 'development',
 
+  devServer: {
+    open: true,
+    hot: true,
 
-    mode: 'development',
-
-    devServer: {
-
-        open: true,
-        hot: true,
-        openPage: '',
-
-
-        client: {
-            logging: 'error'
-        },
-
-        static: [{
-            directory: path.join(__dirname, '../', 'public'),
-            watch: true,
-
-
-        }],
-
-
-        host: ip.address(),
-        port: 8080,
-        // contentBase: './dist'
-
-
-
+    client: {
+      logging: 'error',
     },
 
-    plugins: [
+    static: [
+      {
+        directory: path.join(__dirname, '../', 'public'),
+        watch: true,
+      },
+    ],
 
-        new webpack.HotModuleReplacementPlugin(),
-    ]
+    host: ip.address(),
+    port: 8080,
+    // contentBase: './dist'
+  },
+
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 });
--- configuration/webpack.prod.config.js
+++ configuration/webpack.prod.config.js
@@ -1,27 +1,21 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const webpackConfiguration = require('../webpack.config');
 
 module.exports = merge(webpackConfiguration, {
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, '../', 'dist'),
+    filename: '[name].js',
+  },
 
-
-    mode: 'production',
-    output: {
-        path: path.resolve(__dirname, '../', 'dist'),
-        filename: '[name]-[contenthash].js',
-
-    },
-
-    module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-        }, ]
-    }
-
-
-
-
-
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
 });
