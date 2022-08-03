2f95c4c9c5ad1e161f56d836022ff459cc64c62f
Grzegorz Dubiel <greg2012201@gmail.com>
2022-08-03 15:20:23 +0200

    Remove not required components.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import './sass/index.scss';
-import './components/menu';
-import './components/form';
-import './components/header';
-import './components/accordion';
-import './components/popup';
-import './tools/tools';
-
+import "./sass/index.scss";
+import "./components/menu";
+import "./components/header";
+import "./components/accordion";
+import "./tools/tools";
 
 if (module.hot) {
-    module.hot.accept();
+  module.hot.accept();
 }
--- src/tools/tools.js
+++ src/tools/tools.js
@@ -1,19 +1,19 @@
-import {animationStopperInit} from './resizeAnimationStopper';
-import {isElementInSight} from './isElementsInSight';
+import { animationStopperInit } from "./resizeAnimationStopper";
+import { isElementInSight } from "./isElementsInSight";
 
-const images = document.querySelectorAll('.article__image');
-const text = document.querySelectorAll('.article__content');
-const title = document.querySelector('.slogan__title');
-const motto = document.querySelector('.slogan__motto');
-const page = document.querySelector('.page');
+const images = document.querySelectorAll(".article__image");
+const text = document.querySelectorAll(".article__content");
+const title = document.querySelector(".slogan__title");
+const motto = document.querySelector(".slogan__motto");
+const page = document.querySelector(".page");
 
 const desktopScroll = {
-  duration: '1000ms',
-  ease: 'cubic-bezier(0.23, 1, 0.32, 1)',
+  duration: "1000ms",
+  ease: "cubic-bezier(0.23, 1, 0.32, 1)",
 };
 const mobileScroll = {
-  duration: '0ms',
-  ease: 'linear',
+  duration: "0ms",
+  ease: "linear",
 };
 
 const desktopVieport = 1281;
